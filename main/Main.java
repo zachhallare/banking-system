11d4b52f6721107d0e0e557c89cec4109c794abb
zachhallare <zachhallare@gmail.com>
2025-05-18 16:38:04 +0800

    .

--- main/Main.java
+++ main/Main.java
@@ -110,7 +110,7 @@ public class Main {
         int numOfPinAttempts = 0;
 
         while (numOfPinAttempts < 3) {
-            System.out.print("\n\nEnter current PIN (Six Digits [100000 - 999999]): ");
+            System.out.print("\nEnter current PIN (Six Digits [100000 - 999999]): ");
             oldPin = scanner.nextInt();
 
             if (oldPin != account.getPinNum()) {
@@ -165,10 +165,9 @@ public class Main {
         double currentDeposit = 0; 
         double runningDeposit = 0;
         char moreDeposits;         
-        boolean continueDeposit = true;
-        boolean depositWasMade = false;
+        boolean keepDepositing = true;
 
-        while (continueDeposit) {
+        while (keepDepositing) {
             System.out.println("Choose your deposit denomination:");
             System.out.println("[1] - P100");
             System.out.println("[2] - P200");
@@ -180,7 +179,7 @@ public class Main {
 
             if (denominationNum == 0) {
                 System.out.printf("Deposit Canceled. Your balance is still P%.2f.\n", account.getBalance());
-                continueDeposit = false;    // Stops depositing process.
+                keepDepositing = false;    
             }
 
             int denominationVal;
@@ -205,7 +204,6 @@ public class Main {
 
             currentDeposit = denominationVal * numberOfBills;
             runningDeposit += currentDeposit;
-            depositWasMade = true;
 
             System.out.printf("Current Deposit: P%.2f\n", currentDeposit);
             System.out.printf("Running Deposit: P%.2f\n", runningDeposit);
@@ -214,20 +212,48 @@ public class Main {
             moreDeposits = scanner.next().toLowerCase().charAt(0);
 
             if (moreDeposits != 'y') {
-                continueDeposit = false;    
+                keepDepositing = false;    
             }
         }    
 
-        if (depositWasMade) {
-            double newBalance = account.getBalance() + runningDeposit;
-            account.setBalance(newBalance);
-            System.out.printf("Deposit Successful! Your new balance is P%.2f.\n\n", newBalance);
-            // addTransaction(aTransactionData, pTransactionCount, pTransactionNumber, 'D', +fRunningDeposit, *pBalance);
-        }
+        account.setBalance(account.getBalance() + runningDeposit);
+        System.out.printf("Deposit Successful! Your new balance is P%.2f.\n\n", account.getBalance());
+        // addTransaction(aTransactionData, pTransactionCount, pTransactionNumber, 'D', +fRunningDeposit, *pBalance);
     }
 
 
     public static void withdraw(Scanner scanner, Account account, String[][] transactions) {
+        int withdrawalAmount = 0;     
+        boolean isWithdrawing = true;
+
+        while (isWithdrawing) {
+            System.out.print("\nHow much do you wish to withdraw? \nInput '0' to cancel (Intervals of P100 only): ");
+            withdrawalAmount = scanner.nextInt();
+
+            if (withdrawalAmount == 0) {
+                System.out.println("Transaction is canceled.\n");
+                isWithdrawing = false;
+            }
+            else if (withdrawalAmount < 0) {
+                System.out.println("You cannot withdraw a negative amount.\n");
+            }
+            else if (withdrawalAmount > account.getBalance()) {
+                System.out.printf("Sorry, your balance is insufficient.\nYour current balance is P%.2f.\n\n", account.getBalance());
+            }
+            else if (withdrawalAmount % 100 != 0) {
+                System.out.println("You can only withdraw in intervals of P100.00.\n");
+            }
+            else {
+                isWithdrawing = false; 
+            }
+        }     
+
+        if (withdrawalAmount > 0) {
+            account.setBalance(account.getBalance() - withdrawalAmount);
+            System.out.printf("\nP%d.00 has been withdrawn.\n", withdrawalAmount);
+            System.out.printf("Your balance is P%.2f.\n", account.getBalance());
+            // addTransaction(aTransactionData, pTransactionCount, pTransactionNumber, 'W', -nWithdrawalAmount, *pBalance);
+        }
     }
 
     // // Do this last after the rest are done.
